816166a7d8146c7b923050003b110457efd6a33f
ItMeansBigMountain <48029592+ItMeansBigMountain@users.noreply.github.com>
2026-08-25 15:30:46 +0000

    chore: map CombatAtlas as standalone submodule

--- projects/CombatAtlas/combatAtlas_Backend/combatAtlas_Backend/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MartialArt, DrillCategory, DrillExercise
-
-admin.site.register(MartialArt)
-admin.site.register(DrillCategory)
-admin.site.register(DrillExercise)
